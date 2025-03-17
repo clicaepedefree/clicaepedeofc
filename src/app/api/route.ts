6aa2edfb965e7b6bf03502134046636764697f74
Luan Naufal <luan.naufal@gmail.com>
2025-03-17 07:26:13 -0300

    chore: remove old code

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,3 @@
-import { db } from '@/services/db'
-import { usersTable } from '@/services/db/schema'
-
 export async function GET() {
-  const allUsers = await db.select().from(usersTable)
-  console.log('allUsers', allUsers)
-  return Response.json({ message: allUsers })
+  return Response.json({ message: 'Hello World' })
 }
