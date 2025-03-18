4d6a6d676ffe46a4c5d7cbb3130ad2f3c2c844ae
Luan Naufal <luan.naufal@gmail.com>
2025-03-18 09:39:34 -0300

    feat: add base admin to app (ref CP-19)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from '@/shared/app-sidebar'
+import { AppSidebar } from "@/shared/app-sidebar"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,9 +6,13 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from '@/shared/breadcrumb'
-import { Separator } from '@/shared/separator'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/shared/sidebar'
+} from "@/shared/breadcrumb"
+import { Separator } from "@/shared/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/shared/sidebar"
 
 export default function Page() {
   return (
@@ -17,11 +21,16 @@ export default function Page() {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
               </BreadcrumbItem>
               <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem>
@@ -32,11 +41,11 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div>
       </SidebarInset>
     </SidebarProvider>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,17 @@ import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserBut
 import { ptBR } from '@clerk/localizations'
 import { Button } from '@/shared/button'
 import { PostHogProvider } from '@/services/product-management/provider'
+import { AppSidebar } from '@/shared/app-sidebar'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/shared/breadcrumb'
+import { Separator } from '@/shared/separator'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/shared/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,19 +29,41 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body className={inter.className}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton>
-                <Button variant="outline">Entrar</Button>
-              </SignInButton>
-              <SignUpButton>
-                <Button>Criar conta</Button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton appearance={{ elements: { userButtonPopoverFooter: { display: 'none' } } }} />
-            </SignedIn>
-          </header>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
+                <div className="flex items-center gap-2">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator orientation="vertical" className="mr-2 h-4" />
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem className="hidden md:block">
+                        <BreadcrumbLink href="#">Administrador de lojas</BreadcrumbLink>
+                      </BreadcrumbItem>
+                      <BreadcrumbSeparator className="hidden md:block" />
+                      <BreadcrumbItem>
+                        <BreadcrumbPage>Dashboard</BreadcrumbPage>
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                </div>
+                <div>
+                  <SignedOut>
+                    <SignInButton>
+                      <Button variant="outline">Entrar</Button>
+                    </SignInButton>
+                    <SignUpButton>
+                      <Button>Criar conta</Button>
+                    </SignUpButton>
+                  </SignedOut>
+                  <SignedIn>
+                    <UserButton appearance={{ elements: { userButtonPopoverFooter: { display: 'none' } } }} />
+                  </SignedIn>
+                </div>
+              </header>
+            </SidebarInset>
+          </SidebarProvider>
           <PostHogProvider>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">{children}</main>
           </PostHogProvider>
