c95104a4859c0f663285c6ee5ddd0c269c410e9f
Luan Naufal <luan.naufal@gmail.com>
2025-03-20 08:42:06 -0300

    feat: add DB query to get store configurations

--- src/features/store/api.ts
+++ src/features/store/api.ts
@@ -1,5 +1,31 @@
 'use server'
 import { db } from '@/services/db'
+import { configurationsTable } from '@/services/db/schema/configurations'
 import { storesTable } from '@/services/db/schema/store'
+import { storeConfigurationsTable } from '@/services/db/schema/store-configurations'
+import { coalesce } from '@/services/db/utils'
+import { eq, and } from 'drizzle-orm'
 
 export const getAvailableStores = async () => await db.select().from(storesTable)
+
+export const getStoreConfigurations = async (storeId: string) => {
+  const result = await db
+    .select({
+      id: configurationsTable.id,
+      category: configurationsTable.category,
+      name: configurationsTable.name,
+      type: configurationsTable.type,
+      value: coalesce<string | null>(storeConfigurationsTable.value, configurationsTable.default),
+      createdAt: storeConfigurationsTable.createdAt,
+      updatedAt: storeConfigurationsTable.updatedAt,
+    })
+    .from(configurationsTable)
+    .leftJoin(
+      storeConfigurationsTable,
+      and(
+        eq(configurationsTable.id, storeConfigurationsTable.configurationId),
+        eq(storeConfigurationsTable.storeId, Number(storeId))
+      )
+    )
+  return result
+}
