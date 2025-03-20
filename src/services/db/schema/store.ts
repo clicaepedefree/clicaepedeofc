a138f1e0af220f54e75550674217fc53fd8b5027
Luan Naufal <luan.naufal@gmail.com>
2025-03-20 07:54:59 -0300

    refactor: abstract and separate createdAt and updatedAt

--- src/services/db/schema/store.ts
+++ src/services/db/schema/store.ts
@@ -1,17 +1,12 @@
-import { sql } from 'drizzle-orm'
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, serial, text } from 'drizzle-orm/pg-core'
+import { createdAt, updatedAt } from './utils'
 
 export const storesTable = pgTable('stores', {
   id: serial('id').primaryKey(),
   subdomain: text('subdomain').unique().notNull(),
   name: text('name').notNull(),
-  createdAt: timestamp('created_at')
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: timestamp('updated_at')
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull()
-    .$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+  createdAt,
+  updatedAt,
 })
 
 export type InsertStore = typeof storesTable.$inferInsert
---
+++ src/services/db/schema/utils.ts
@@ -0,0 +1,11 @@
+import { sql } from 'drizzle-orm'
+import { timestamp } from 'drizzle-orm/pg-core'
+
+export const createdAt = timestamp('created_at')
+  .notNull()
+  .default(sql`CURRENT_TIMESTAMP`)
+
+export const updatedAt = timestamp('updated_at')
+  .default(sql`CURRENT_TIMESTAMP`)
+  .notNull()
+  .$onUpdate(() => sql`CURRENT_TIMESTAMP`)
