f4327f2dde890f5f2d4547eb03075c75e28ac7c3
Luan Naufal <luan.naufal@gmail.com>
2025-03-21 12:15:40 -0300

    feat: force rerendering on changing stores

--- src/app/(admin)/settings/page.tsx
+++ src/app/(admin)/settings/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import { useStoreConfigurations } from '@/features/store/hooks/use-store-configurations'
 import { ConfigurationCategory } from '@/features/store/components/store-configuration-category'
+import { selectedStoreIdAtom } from '@/features/store/state'
+import { useAtom } from 'jotai'
 
 export default function Page() {
+  const [selectedStoreId] = useAtom(selectedStoreIdAtom)
   const { configurationsByCategory } = useStoreConfigurations()
 
   return (
-    <div className="grid auto-rows-min gap-x-4 gap-y-10  md:grid-cols-3">
+    <div key={selectedStoreId} className="grid auto-rows-min gap-x-4 gap-y-10  md:grid-cols-3">
       {configurationsByCategory.map(({ category, configurations }) => (
         <ConfigurationCategory key={category} category={category} configurations={configurations} />
       ))}
--- src/features/store/components/configuration-switch.tsx
+++ src/features/store/components/configuration-switch.tsx
@@ -1,17 +1,15 @@
 'use client'
-import { useState } from 'react'
 import { Switch } from '@/shared/switch'
 import { StoreConfiguration } from '@/features/store/types'
 import { Label } from '@/shared/label'
 
 export const ConfigurationSwitch = ({ configuration }: { configuration: StoreConfiguration }) => {
-  const [configurationValue, setConfigurationValue] = useState(configuration.value === 'true')
   const configLabel =
     configNameToLabelMapping[configuration.name as keyof typeof configNameToLabelMapping] ?? configuration.name
   return (
     <div className="flex items-center space-x-2">
       <Label>
-        <Switch checked={configurationValue} onCheckedChange={setConfigurationValue} />
+        <Switch defaultChecked={configuration.value === 'true'} />
         <span>{configLabel}</span>
       </Label>
     </div>
