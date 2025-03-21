ee68e74d3c8a05160c7f8dbf8f8f6f42bce17171
Luan Naufal <luan.naufal@gmail.com>
2025-03-21 10:14:26 -0300

    feat: add typography components

---
+++ src/shared/typography/h2.tsx
@@ -0,0 +1,3 @@
+export const H2 = ({ children }: { children: React.ReactNode }) => {
+  return <h2 className="scroll-m-20 pb-2 text-3xl font-normal tracking-tight first:mt-0">{children}</h2>
+}
---
+++ src/shared/typography/lead-text.tsx
@@ -0,0 +1,3 @@
+export const LeadText = ({ children }: { children: React.ReactNode }) => {
+  return <p className="text-xl text-muted-foreground">{children}</p>
+}
