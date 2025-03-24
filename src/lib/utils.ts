9f8cce13ece84e41da46d1efe6199e9bce9ca26c
Luan Naufal <luan.naufal@gmail.com>
2025-03-23 22:16:45 -0300

    feat: add tryCatch util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,24 @@ export function cn(...inputs: ClassValue[]) {
 
 export const isServer = isServerReactQuery
 export const isClient = !isServer
+
+export type Success<T> = {
+  data: T
+  error: null
+}
+
+export type Failure<E> = {
+  data: null
+  error: E
+}
+
+export type Result<T, E = Error> = Success<T> | Failure<E>
+
+export async function tryCatch<T, E = Error>(promise: Promise<T>): Promise<Result<T, E>> {
+  try {
+    const data = await promise
+    return { data, error: null }
+  } catch (error) {
+    return { data: null, error: error as E }
+  }
+}
