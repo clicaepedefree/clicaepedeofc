0c1c39666fd3187cf1e621e383acebec50365bc5
Luan Naufal <luan.naufal@gmail.com>
2025-04-22 10:00:31 -0300

    feat: seed base catalogs

---
+++ supabase/migrations/0005_populate base catalogs.sql
@@ -0,0 +1,4 @@
+-- Custom SQL migration file, put your code below! --
+INSERT INTO "catalogs" (name) VALUES ('POS');
+INSERT INTO "catalogs" (name) VALUES ('INDOOR');
+INSERT INTO "catalogs" (name) VALUES ('DELIVERY');
