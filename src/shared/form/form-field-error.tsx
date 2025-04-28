f9bc36bb7a11dbcabbf440cadbaba6502dcc8848
Luan Naufal <luan.naufal@gmail.com>
2025-04-28 10:34:34 -0300

    feat: update form fiel and add textarea components

--- src/shared/form/form-field-error.tsx
+++ src/shared/form/form-field-error.tsx
@@ -6,10 +6,10 @@ export const FormFieldError = ({ field }: { field: AnyFieldApi }) => {
   if (!hasErrors) return null
 
   return (
-    <em className="text-red-500 text-xs">
+    <span className="text-red-500 text-xs">
       {errors.map((error, index) => (
         <div key={`${field.name}-error-${index}`}>{error.message}</div>
       ))}
-    </em>
+    </span>
   )
 }
