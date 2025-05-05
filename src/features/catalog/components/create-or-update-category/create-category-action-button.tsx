38f2851c66cff4f23c7c491ca4e2f493666698d7
Luan Naufal <luan.naufal@gmail.com>
2025-05-04 22:44:19 -0300

    feat: add base sidebar form

--- src/features/catalog/components/create-or-update-category/create-category-action-button.tsx
+++ src/features/catalog/components/create-or-update-category/create-category-action-button.tsx
@@ -1,59 +1,35 @@
 'use client'
 
-import { CreateCategoryForm } from '@/features/catalog/components/create-or-update-category/create-category.form'
-// import { CreateOrUpdateCategoryForm } from '@/features/catalog/components/create-or-update-category/create-or-update-category.form'
+import { CreateOrUpdateCategoryForm } from '@/features/catalog/components/create-or-update-category/create-or-update-category.form'
 import { Category } from '@/features/catalog/types'
 import { Button } from '@/shared/button'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/shared/sheet'
+import { BaseSideBarActionForm } from '@/shared/form/base-side-bar-action-form'
 import { Plus } from 'lucide-react'
-import { useState } from 'react'
 
 type CreateCategoryActionButtonProps = {
   onSuccess?: (newCategory: Category) => void
 }
 export const CreateCategoryActionButton = ({ onSuccess }: CreateCategoryActionButtonProps) => {
-  const [open, setOpen] = useState(false)
-
-  const onNewCategoryCreated = (newCategory: Category) => {
-    onSuccess?.(newCategory)
-    setOpen(false)
-  }
   return (
-    <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger asChild>
+    <BaseSideBarActionForm
+      title="Nova categoria"
+      description="Preencha as informações da nova categoria."
+      trigger={
         <Button variant="default" className="font-semibold">
           <Plus size={24} strokeWidth={3} /> Categoria
         </Button>
-      </SheetTrigger>
-      <SheetContent
-        className="w-11/12 sm:max-w-5xl max-h-dvh h-fit rounded-l-2xl"
-        onPointerDownOutside={() => {}}
-        onInteractOutside={event => event.preventDefault()}
-        onFocusOutside={() => {}}
-      >
-        <SheetHeader>
-          <SheetTitle>Nova categoria</SheetTitle>
-          <SheetDescription>Preencha as informações da nova categoria.</SheetDescription>
-        </SheetHeader>
-        {/* <CreateOrUpdateCategoryForm
+      }
+    >
+      {({ FooterContainer, closeSidebar }) => (
+        <CreateOrUpdateCategoryForm
           className="px-4 overflow-y-auto relative"
-          FooterContainerComponent={SheetFooter}
-          onSuccess={onNewCategoryCreated}
-        /> */}
-        <CreateCategoryForm
-          className="px-4 overflow-y-auto relative"
-          FooterContainerComponent={SheetFooter}
-          onSuccess={onNewCategoryCreated}
+          onSuccess={category => {
+            onSuccess?.(category)
+            closeSidebar?.()
+          }}
+          FooterContainerComponent={FooterContainer}
         />
-      </SheetContent>
-    </Sheet>
+      )}
+    </BaseSideBarActionForm>
   )
 }
---
+++ src/shared/form/base-side-bar-action-form.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/shared/sheet'
+import { useState } from 'react'
+
+export type SidebarActionFormChildrenProps = {
+  FooterContainer: typeof SheetFooter
+  closeSidebar(): void
+}
+
+export type BaseSideBarActionFormProps = {
+  title: string
+  description?: string
+  children: (props: SidebarActionFormChildrenProps) => React.ReactNode
+  trigger: React.ReactNode
+}
+
+export const BaseSideBarActionForm = ({ children, title, description, trigger }: BaseSideBarActionFormProps) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>{trigger}</SheetTrigger>
+      <SheetContent
+        className="w-11/12 sm:max-w-5xl max-h-dvh h-fit rounded-l-2xl"
+        onPointerDownOutside={() => {}}
+        onInteractOutside={event => event.preventDefault()}
+        onFocusOutside={() => {}}
+      >
+        <SheetHeader>
+          <SheetTitle>{title}</SheetTitle>
+          {description && <SheetDescription>{description}</SheetDescription>}
+        </SheetHeader>
+        {children({ FooterContainer: SheetFooter, closeSidebar: () => setOpen(false) })}
+      </SheetContent>
+    </Sheet>
+  )
+}
