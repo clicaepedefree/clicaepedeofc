110bf1cbfc9c1a5857a91ab9026274f4ad4ca6f0
Luan Naufal <luan.naufal@gmail.com>
2025-05-22 16:01:48 -0300

    fix: prices column types

---
+++ supabase/migrations/0009_Update price columns types for decimal.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "category_products" ALTER COLUMN "price" SET DATA TYPE numeric(19, 4);--> statement-breakpoint
+ALTER TABLE "category_products" ALTER COLUMN "original_price" SET DATA TYPE numeric(19, 4);
