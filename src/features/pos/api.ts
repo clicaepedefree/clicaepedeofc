e2187f5f2d7abce71fa7b04c3a41f3ac2093af4a
Luan Naufal <luan.naufal@gmail.com>
2025-07-05 00:25:08 -0300

    feat: add isAvailable based on existing open session

--- src/features/pos/api.ts
+++ src/features/pos/api.ts
@@ -2,15 +2,31 @@
 
 import { validateUserPermissionsForStore } from '@/features/store/api'
 import { db } from '@/services/db'
-import { countersTable, InsertCounter } from '@/services/db/schema'
-import { eq } from 'drizzle-orm'
+import {
+  counterSessionsTable,
+  countersTable,
+  InsertCounter,
+} from '@/services/db/schema'
+import { and, eq, getTableColumns, sql } from 'drizzle-orm'
 
 export const listCounters = async (storeId: number) => {
   await validateUserPermissionsForStore(storeId, 'admin')
 
-  return await db.query.countersTable.findMany({
-    where: eq(countersTable.storeId, storeId),
-  })
+  return await db
+    .select({
+      ...getTableColumns(countersTable),
+      isAvailable: sql<boolean>`count(${counterSessionsTable.id}) = 0`,
+    })
+    .from(countersTable)
+    .leftJoin(
+      counterSessionsTable,
+      and(
+        eq(countersTable.id, counterSessionsTable.counterId),
+        eq(counterSessionsTable.status, 'OPEN')
+      )
+    )
+    .where(eq(countersTable.storeId, storeId))
+    .groupBy(countersTable.id)
 }
 
 export const createCounter = async (newStore: InsertCounter) => {
