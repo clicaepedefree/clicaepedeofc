41d1fe004da3c502e304e18bf34e5448491ec164
Luan Naufal <luan.naufal@gmail.com>
2025-07-06 13:36:57 -0300

    chore: rename setup for db debugger

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -6,7 +6,11 @@ import * as dbSchema from './schema'
 config({ path: '.env.local' })
 
 const connectionString = process.env.POSTGRES_URL
-const isDebugDbQueries = process.env.DRIZZLE_DEBUG === 'true'
+const enableDbQueriesDebugger = process.env.DRIZZLE_DEBUG === 'true'
 
 const client = postgres(connectionString!)
-export const db = drizzle({ schema: dbSchema, client, logger: isDebugDbQueries })
+export const db = drizzle({
+  schema: dbSchema,
+  client,
+  logger: enableDbQueriesDebugger,
+})
