05a5eb3fd6b27cb59d6a2a7ee42a94f7d9795ed9
Luan Naufal <luan.naufal@gmail.com>
2025-08-26 12:26:20 -0300

    feat: update title pages on open page

--- src/app/(admin)/settings/[settingId]/page.tsx
+++ src/app/(admin)/settings/[settingId]/page.tsx
@@ -4,13 +4,22 @@ import { PageHeaderBlock } from '@/shared/blocks/page-header-block'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/tabs'
 import { useParams, useRouter } from 'next/navigation'
 
+const settingsPageToTitleMapping = {
+  store: 'Configuração da loja',
+  fiscal: 'Configuração fiscal',
+}
+
 export default function SettingsPage() {
   const router = useRouter()
-  const { settingId } = useParams<{ settingId?: string }>()
+  const { settingId = 'store' } = useParams<{ settingId?: string }>()
 
+  const pageTitle =
+    settingsPageToTitleMapping[
+      settingId as keyof typeof settingsPageToTitleMapping
+    ] ?? undefined
   return (
     <>
-      <PageHeaderBlock title="Configurações" subtitle="Configurações da loja" />
+      <PageHeaderBlock title="Configurações" subtitle={pageTitle} />
       <Tabs
         value={settingId ?? 'store'}
         onValueChange={settingId => router.replace(`/settings/${settingId}`)}
