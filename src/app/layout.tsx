93177ffb5f0193cfb3786bc9258ad74150f53622
Bruno Giusti <muller_bru@hotmail.com>
2026-07-29 22:43:30 -0300

    fix: corrige remocao do carrinho mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,26 @@
 import '@/app/globals.css'
 import Providers from '@/app/providers'
+import { cn } from '@/shared/lib/utils'
+import { Inter } from 'next/font/google'
+import type { Viewport } from 'next'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <Providers>{children}</Providers>
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={cn(inter.className, 'h-dvh text-foreground')}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,40 +1,28 @@
 'use client'
 import { getQueryClient } from '@/services/query-client'
-import { cn } from '@/shared/lib/utils'
 import { Toaster } from '@/shared/sonner'
 import { ptBR } from '@clerk/localizations'
 import { ClerkProvider } from '@clerk/nextjs'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Provider as JotaiProvider } from 'jotai'
-import { Inter } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Providers({
   children,
-  bodyClassName,
 }: {
   children: React.ReactNode
-  bodyClassName?: string
 }) {
   const queryClient = getQueryClient()
 
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
-      <body
-        className={cn(inter.className, 'h-dvh text-foreground', bodyClassName)}
-      >
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <QueryClientProvider client={queryClient}>
-            <Toaster />
-            <JotaiProvider>{children}</JotaiProvider>
-            <ReactQueryDevtools />
-          </QueryClientProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <JotaiProvider>{children}</JotaiProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
